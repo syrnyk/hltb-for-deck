2f46bca730dba708825a5a859d047c5439a60c07
syrnyk <30251275+syrnyk@users.noreply.github.com>
2025-01-01 01:42:07 +0100

    fix: normalize method now removes multiple whitespaces for consistency

--- src/utils.ts
+++ src/utils.ts
@@ -4,5 +4,6 @@ export const normalize = (str: string) => {
         .normalize('NFD')
         .replace(/[\u0300-\u036f]/g, '')
         .replace(/[^a-zA-Z0-9\s]/g, ' ') // replacing any special char with a space instead of an empty string should avoid game names like "NieR:Automata™" to end up as "NieRAutomata" which will fail in HLTB search
+        .replace(/\s\s+/g, ' ') // replacing multiple whitespaces by a single space for consistency
         .trim();
 };
